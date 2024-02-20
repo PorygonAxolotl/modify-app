78f8a92a9bd9f209420da49c982d0d6c45ed3752
PorygonAxolotl <97900480+PorygonAxolotl@users.noreply.github.com>
2024-02-20 10:02:18 -0400

    shit 1

---
+++ index.js
@@ -0,0 +1,6 @@
+<button class="modify-button" onclick="animateButton()">Modify Here!</button>
+
+function animateButton() {
+    document.querySelector(".modify-button").classList.add("animated");
+  }
+  
